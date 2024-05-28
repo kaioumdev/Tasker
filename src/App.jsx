7d9b9b28dc83ad3ea94e565df048f2261a215631
kaiyumdev <afrankaiyum34@gmail.com>
2024-05-28 15:01:19 +0600

    feat(heroSection): complete heroSection & Implements dark theme with this project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from './Header/Header'
 import Footer from './Footer/Footer'
+import HeroSection from './HeroSection'
 
 const App = () => {
   return (
     <div>
       <Header></Header>
+      <HeroSection></HeroSection>
       <Footer></Footer>
     </div>
   )
