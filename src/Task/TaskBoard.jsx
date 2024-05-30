f4218c023d02d12b9d3a71ebcbaf5e33a15e6518
kaiyumdev <afrankaiyum34@gmail.com>
2024-05-30 15:42:52 +0600

    feat(addTask): create addTask function

--- src/Task/TaskBoard.jsx
+++ src/Task/TaskBoard.jsx
@@ -13,6 +13,10 @@ const TaskBoard = () => {
         "isFavorite": false
     }
     const [tasks, setTasks] = useState([defaultTask])
+
+    const handleAddTask = () => {
+        console.log("Click add task")
+    }
     return (
         <section className="mb-20" id="tasks">
             <div className="container">
@@ -21,7 +25,7 @@ const TaskBoard = () => {
                 </div>
 
                 <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-                    <TaskActions></TaskActions>
+                    <TaskActions onAddClick={handleAddTask}></TaskActions>
                     <TaskList tasks={tasks}></TaskList>
                 </div>
             </div>
