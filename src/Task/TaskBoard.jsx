589ea962d44e928cce93dd3e91dfbb6a328d942b
kaiyumdev <afrankaiyum34@gmail.com>
2024-05-30 11:49:57 +0600

    feat(taskActions): create taskActions UI successfully

--- src/Task/TaskBoard.jsx
+++ src/Task/TaskBoard.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
 import SearchTask from './SearchTask'
+import TaskActions from './TaskActions'
 
 const TaskBoard = () => {
-  return (
-    <section className="mb-20" id="tasks">
-    <div className="container">
-        <div className="p-2 flex justify-end">
-            <SearchTask></SearchTask>
-        </div>
+    return (
+        <section className="mb-20" id="tasks">
+            <div className="container">
+                <div className="p-2 flex justify-end">
+                    <SearchTask></SearchTask>
+                </div>
 
-        <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-        </div>
-    </div>
-</section>
-  )
+                <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
+                    <TaskActions></TaskActions>
+                </div>
+            </div>
+        </section>
+    )
 }
 
 export default TaskBoard
