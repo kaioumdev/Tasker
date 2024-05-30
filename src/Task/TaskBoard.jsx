2d6dce58f6732f9d82bec1e860c92806cd9d5b47
kaiyumdev <afrankaiyum34@gmail.com>
2024-05-30 11:40:46 +0600

    feat(searchTask): Create searchTask UI successfully

--- src/Task/TaskBoard.jsx
+++ src/Task/TaskBoard.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import SearchTask from './SearchTask'
 
 const TaskBoard = () => {
   return (
     <section className="mb-20" id="tasks">
     <div className="container">
         <div className="p-2 flex justify-end">
+            <SearchTask></SearchTask>
         </div>
 
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
