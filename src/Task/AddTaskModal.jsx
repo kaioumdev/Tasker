f227fdd2bf17f36ee570f216f586e638b1bf8330
kaiyumdev <afrankaiyum34@gmail.com>
2024-05-30 20:26:41 +0600

    feat(modalData): Implements functionality to get modal form data to set data at modalTask state

--- src/Task/AddTaskModal.jsx
+++ src/Task/AddTaskModal.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const AddTaskModal = () => {
+    const [task, setTask] = useState({
+        title: "",
+        description: "",
+        tags: [],
+        priority: "",
+        isFavorite: false
+    })
+
+    const handleChange = (e) => {
+        const name = e.target.name;
+        let value = e.target.value;
+        if(name === "tags"){
+            value = value.split(",")
+        }
+
+        setTask({
+            ...task,
+            [name]: value
+        })
+    }
     return (
         <>
-        <div className='bg-black bg-opacity-70 h-full w-full z-10 absolute top-0 left-0'></div>
+            <div className='bg-black bg-opacity-70 h-full w-full z-10 absolute top-0 left-0'></div>
             <form className="mx-auto my-10 w-full max-w-[740px] rounded-xl border border-[#FEFBFB]/[36%] bg-[#191D26] p-9 max-md:px-4 lg:my-20 lg:p-11 z-10 absolute top-1/4 left-1/3">
                 <h2 className="mb-9 text-center text-2xl font-bold text-white lg:mb-11 lg:text-[28px]">
                     {/* {isAdd ? "Add New Task" : "Edit Task"} */}
@@ -17,7 +37,8 @@ const AddTaskModal = () => {
                             type="text"
                             name="title"
                             id="title"
-                            // value={task.title}
+                            value={task.title}
+                            onChange={handleChange}
                             required
                         />
                     </div>
@@ -29,7 +50,8 @@ const AddTaskModal = () => {
                             type="text"
                             name="description"
                             id="description"
-                            // value={task.description}
+                            value={task.description}
+                            onChange={handleChange}
                             required
                         ></textarea>
                     </div>
@@ -42,7 +64,8 @@ const AddTaskModal = () => {
                                 type="text"
                                 name="tags"
                                 id="tags"
-                                // value={task.tags}
+                                value={task.tags}
+                                onChange={handleChange}
                                 required
                             />
                         </div>
@@ -53,7 +76,8 @@ const AddTaskModal = () => {
                                 className="block w-full cursor-pointer rounded-md bg-[#2D323F] px-3 py-2.5"
                                 name="priority"
                                 id="priority"
-                                // value={task.priority}
+                                value={task.priority}
+                                onChange={handleChange}
                                 required
                             >
                                 <option value="">Select Priority</option>
