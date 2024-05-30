95d25758609ebf0f205782d91437f100ebeb1c8c
kaiyumdev <afrankaiyum34@gmail.com>
2024-05-30 11:35:42 +0600

    feat(taskBoard): create taskBoard component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import Header from './Header/Header'
 import Footer from './Footer/Footer'
 import HeroSection from './HeroSection'
+import TaskBoard from './Task/TaskBoard'
 
 const App = () => {
   return (
     <div>
       <Header></Header>
+      <div className='flex flex-col justify-center items-center'>
       <HeroSection></HeroSection>
+      <TaskBoard></TaskBoard>
+      </div>
       <Footer></Footer>
     </div>
   )
---
+++ src/Task/TaskBoard.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const TaskBoard = () => {
+  return (
+    <section className="mb-20" id="tasks">
+    <div className="container">
+        <div className="p-2 flex justify-end">
+        </div>
+
+        <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
+        </div>
+    </div>
+</section>
+  )
+}
+
+export default TaskBoard
