6e0a42988a014912b508ce34b38521fb601e3cc9
kaiyumdev <afrankaiyum34@gmail.com>
2024-05-31 12:10:12 +0600

    feat(noTaskFound): Implements noTaskFound component

---
+++ src/Task/NoTaskFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NoTaskFound = () => {
+  return (
+    <p className="text-center text-3xl">
+    No Tasks Found. Please Add One.
+</p>
+  )
+}
+
+export default NoTaskFound
--- src/Task/TaskBoard.jsx
+++ src/Task/TaskBoard.jsx
@@ -3,6 +3,7 @@ import SearchTask from './SearchTask'
 import TaskActions from './TaskActions'
 import TaskList from './TaskList'
 import AddTaskModal from './AddTaskModal'
+import NoTaskFound from './NoTaskFound'
 
 const TaskBoard = () => {
     const defaultTask = {
@@ -18,19 +19,19 @@ const TaskBoard = () => {
     const [taskToUpdate, setTaskToUpdate] = useState(null)
 
     const handleAddEditTask = (newTask, isAdd) => {
-        if(isAdd){
+        if (isAdd) {
             setTasks([...tasks, newTask])
-        }else{
+        } else {
             setTasks(
                 tasks.map((task) => {
-                    if(task.id === newTask.id){
+                    if (task.id === newTask.id) {
                         return newTask
                     }
                     return task
                 })
             )
         }
-       setShowAddModal(false)
+        setShowAddModal(false)
     }
 
     const handleCloseClick = () => {
@@ -67,7 +68,7 @@ const TaskBoard = () => {
     return (
         <section className="mb-20" id="tasks">
             {
-                showAddModal && <AddTaskModal onSave={handleAddEditTask} onCloseClick={handleCloseClick}  taskToUpdate={taskToUpdate}></AddTaskModal>
+                showAddModal && <AddTaskModal onSave={handleAddEditTask} onCloseClick={handleCloseClick} taskToUpdate={taskToUpdate}></AddTaskModal>
             }
             <div className="container">
                 <div className="p-2 flex justify-end">
@@ -75,8 +76,10 @@ const TaskBoard = () => {
                 </div>
 
                 <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-                    <TaskActions onAddClick={() => {setShowAddModal(true)}} onDeleteAllClick={handleDeleteAllClick}></TaskActions>
-                    <TaskList tasks={tasks} onEdit={handleEditTask} onDelete={handleDeleteTask} onFav={handleFavorite}></TaskList>
+                    <TaskActions onAddClick={() => { setShowAddModal(true) }} onDeleteAllClick={handleDeleteAllClick}></TaskActions>
+                    {
+                        tasks.length > 0 ? <TaskList tasks={tasks} onEdit={handleEditTask} onDelete={handleDeleteTask} onFav={handleFavorite}></TaskList> : (<NoTaskFound></NoTaskFound>)
+                    }
                 </div>
             </div>
         </section>
