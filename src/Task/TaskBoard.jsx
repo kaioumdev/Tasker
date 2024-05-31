b86a88a9773022ee2e1ba89f380533a25d8ce716
kaiyumdev <afrankaiyum34@gmail.com>
2024-05-31 11:37:08 +0600

    feat(deleteAll): working deleteAll functionality to delete all tasks

--- src/Task/TaskBoard.jsx
+++ src/Task/TaskBoard.jsx
@@ -47,6 +47,11 @@ const TaskBoard = () => {
         const tasksAfterDelete = tasks.filter(task => task.id !== taskId)
         setTasks(tasksAfterDelete)
     }
+
+    const handleDeleteAllClick = () => {
+        tasks.length = 0;
+        setTasks([...tasks])
+    }
     return (
         <section className="mb-20" id="tasks">
             {
@@ -58,7 +63,7 @@ const TaskBoard = () => {
                 </div>
 
                 <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-                    <TaskActions onAddClick={() => {setShowAddModal(true)}}></TaskActions>
+                    <TaskActions onAddClick={() => {setShowAddModal(true)}} onDeleteAllClick={handleDeleteAllClick}></TaskActions>
                     <TaskList tasks={tasks} onEdit={handleEditTask} onDelete={handleDeleteTask}></TaskList>
                 </div>
             </div>
