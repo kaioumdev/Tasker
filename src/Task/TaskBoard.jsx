cd0d47f34c5712b0fe013c4c966eaa2e0a7299c2
kaiyumdev <afrankaiyum34@gmail.com>
2024-10-29 22:10:21 +0600

    feat(favorite): implements favorite icon toggle bar to click to change color

--- src/Task/TaskBoard.jsx
+++ src/Task/TaskBoard.jsx
@@ -53,6 +53,15 @@ const TaskBoard = () => {
         tasks.length = 0;
         setTasks([...tasks]);
     }
+
+    const handleFavorite = (taskId) => {
+        const taskIndex = tasks.findIndex((task) => task.id === taskId);
+
+        const newTasks = [...tasks];
+
+        newTasks[taskIndex].isFavorite = !newTasks[taskIndex].isFavorite;
+        setTasks(newTasks);
+    }
     return (
         <section className="mb-20" id="tasks">
             <div className="container">
@@ -62,7 +71,7 @@ const TaskBoard = () => {
                 {showAddModal && <AddTaskModal onSave={handleAddEditTask} toUpdateTask={toUpdateTask} onCloseClick={handleCloseClick}></AddTaskModal>}
                 <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
                     <TaskActions onAddClick={() => setShowAddModal(true)} onDeleteAllClick={handleDeleteAllClick}></TaskActions>
-                    <TaskList tasks={tasks} onEdit={handleEditTask} onDelete={handleDeleteTask}></TaskList>
+                    <TaskList tasks={tasks} onEdit={handleEditTask} onDelete={handleDeleteTask} onFav={handleFavorite}></TaskList>
                 </div>
             </div>
         </section>
