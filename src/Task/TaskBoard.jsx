b91f8879f5f377fb15bb6d9055000622ccb3c517
kaiyumdev <afrankaiyum34@gmail.com>
2024-10-31 12:34:05 +0600

    feat(showMessage): is there is no tasks to show no task found message

--- src/Task/TaskBoard.jsx
+++ src/Task/TaskBoard.jsx
@@ -72,7 +72,9 @@ const TaskBoard = () => {
                 {showAddModal && <AddTaskModal onSave={handleAddEditTask} toUpdateTask={toUpdateTask} onCloseClick={handleCloseClick}></AddTaskModal>}
                 <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
                     <TaskActions onAddClick={() => { setShowAddModal(true) }} onDeleteAllClick={handleDeleteAllClick}></TaskActions>
-                    <TaskList tasks={tasks} onEdit={handleEditTask} onDelete={handleDeleteTask} onFav={handleFavoriteIcon}></TaskList>
+                    {
+                        tasks.length > 0 ? (<TaskList tasks={tasks} onEdit={handleEditTask} onDelete={handleDeleteTask} onFav={handleFavoriteIcon}></TaskList>) : (<NoTaskFound></NoTaskFound>)
+                    }
                 </div>
             </div>
         </section>
