1b0bf9c694f666aa2667e0eea99fc4e7065d4074
kaiyumdev <afrankaiyum34@gmail.com>
2024-10-31 15:37:28 +0600

    feat(footer): updated footer component to make ui more modern

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,15 +1,31 @@
-import React from 'react'
+// import React from 'react'
+
+// const Footer = () => {
+//     return (
+//         <footer className="py-6 md:py-8">
+//             <div className="container mx-auto">
+//                 <p className="text-center text-xs text-[#6A6A6A] lg:text-sm">
+//                     Copyright ©2024 | All rights reserved by Learn with Sumit
+//                 </p>
+//             </div>
+//         </footer>
+//     )
+// }
+
+// export default Footer
+
+import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className="py-6 md:py-8">
+        <footer className="py-6 md:py-8 bg-[#191D26]">
             <div className="container mx-auto">
-                <p className="text-center text-xs text-[#6A6A6A] lg:text-sm">
+                <p className="text-center text-xs text-gray-400 lg:text-sm">
                     Copyright ©2024 | All rights reserved by Learn with Sumit
                 </p>
             </div>
         </footer>
-    )
+    );
 }
 
-export default Footer
+export default Footer;
