85171f62c449f30b435676fe0a979dc8b86ec891
kaiyumdev <afrankaiyum34@gmail.com>
2024-10-31 15:36:41 +0600

    feat(header): updated header component to make ui more modern

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,20 +1,36 @@
-import React from 'react'
-import TaskLogo from "../../src/assets/task.svg"
+// import React from 'react'
+// import TaskLogo from "../../src/assets/task.svg"
+
+// const Header = () => {
+//     return (
+//         <nav className="py-6 md:py-8 fixed top-0 w-full !bg-[#191D26] z-50">
+//             <div className="container mx-auto flex items-center justify-between gap-x-6">
+//                 <a href="/">
+//                     <img
+//                         className="h-[45px]"
+//                         src={TaskLogo}
+//                         alt="Lws"
+//                     />
+//                 </a>
+//             </div>
+//         </nav>
+//     )
+// }
+
+// export default Header
+import React from 'react';
+import TaskLogo from "../../src/assets/task.svg";
 
 const Header = () => {
     return (
-        <nav className="py-6 md:py-8 fixed top-0 w-full !bg-[#191D26] z-50">
+        <nav className="py-6 md:py-8 fixed top-0 w-full bg-[#191D26] shadow-lg z-50">
             <div className="container mx-auto flex items-center justify-between gap-x-6">
                 <a href="/">
-                    <img
-                        className="h-[45px]"
-                        src={TaskLogo}
-                        alt="Lws"
-                    />
+                    <img className="h-[45px]" src={TaskLogo} alt="Task Logo" />
                 </a>
             </div>
         </nav>
-    )
+    );
 }
 
-export default Header
+export default Header;
